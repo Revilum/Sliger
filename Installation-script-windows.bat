2894c908b088f52fb25c4ec838a1f72e32967d88
Revilum <Revilum@users.noreply.github.com>
2016-06-30 18:45:18 +0200

    Add files via upload

---
+++ Installation-script-windows.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Installing modules...
+npm install
+echo All modules Installed. Make sure that you have nodejs installed.
+echo  You can run the server: Open the src folder and open Start-windows.bat
+echo -----------------------------------------------------------------------
+pause
---
+++ sliger-installation-script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Sliger is installing... Make sure that you have nodejs,
+wget and unzip.
